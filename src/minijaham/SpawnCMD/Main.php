9170d3f790c2dfc4cdc347167ec6db625ddff410
minijaham <60860894+minijaham@users.noreply.github.com>
2020-10-26 14:10:29 -0700

    Added force spawn. (togglable in config)

--- src/minijaham/SpawnCMD/Main.php
+++ src/minijaham/SpawnCMD/Main.php
@@ -7,37 +7,34 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\item\Item;
 use pocketmine\Player;
-use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerLoginEvent;
 
-class Main extends PluginBase implements Listener
-{
+class Main extends PluginBase implements Listener {
     /** @var string[] */
     private $config;
     
-  public function onEnable() : void
-	{
-		$this->config = $this->getConfig()->getAll();
-	}
-  public function onCommand(CommandSender $s, Command $cmd,$label, array $args):bool
-  {
-    $ds = $this->getServer()->getDefaultLevel()->getSpawnLocation();
-    $name = $s->getName();
-    switch($cmd->getName())
-    {
-      case "spawn":
-        if($s instanceof Player)
-        {
-          $s->sendMessage($this->config["onteleport"]);
-          $s->teleport($ds);
-          return true;
+    public function onEnable() : void {
+  	    $this->config = $this->getConfig()->getAll();
+	    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+	    
+    public function onCommand(CommandSender $sender, Command $cmd,$label, array $args) : bool {
+        $spawn = $this->getServer()->getDefaultLevel()->getSpawnLocation();
+        switch($cmd->getName()) {
+            case "spawn":
+                if($sender instanceof Player){
+                    $sender->sendMessage($this->config["onteleport"]);
+                    $sender->teleport($spawn);
+                    return true;
+  	         } else {
+                    $s->sendMessage($this->config["useingame"]);
+                    return true;
+  	    }
         }
-        else
-        {
-            $s->sendMessage($this->config["useingame"]);
-            return true;
-        }
-
-	}
-  }
+    }
+    public function forceSpawn(PlayerLoginEvent $event){
+        if ($this->config["forcespawn"] === "true")
+        $event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+    }
 }
